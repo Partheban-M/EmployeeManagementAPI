be3037095d2660dd6417eddfd8947033628a14e4
partheban-M <partheban61@gmail.com>
2026-05-23 17:34:45 +0530

    create repo emp

---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,65 @@
+using EmployeeManagementAPI.Data;
+using EmployeeManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementAPI.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee?> GetEmployeeById(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<Employee?> UpdateEmployee(int id, Employee updatedEmployee)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return null;
+            }
+
+            employee.Name = updatedEmployee.Name;
+            employee.Department = updatedEmployee.Department;
+            employee.Salary = updatedEmployee.Salary;
+
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<bool> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return false;
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
