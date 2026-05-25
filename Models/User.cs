de75dc3babed6ce3baf7e5efa1c58bf5fd683d10
partheban-M <partheban61@gmail.com>
2026-05-25 16:56:10 +0530

    add role jwt in user

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,7 @@ namespace EmployeeManagementAPI.Models
         public string Username { get; set; } = string.Empty;
 
         public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User";
     }
 }
