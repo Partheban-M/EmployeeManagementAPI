2117ab1384d1ec261e52382dcc90883230bfb10e
partheban-M <partheban61@gmail.com>
2026-05-25 16:44:52 +0530

    add role in jwt

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -74,6 +74,7 @@ namespace EmployeeManagementAPI.Controllers
         {
             var claims = new[]
             {
+                new Claim(ClaimTypes.Role, user.Role),
                 new Claim(ClaimTypes.Name, user.Username),
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
             };
