bb858338cce156f97806cf2cbf7cc21f28e6b671
partheban-M <partheban61@gmail.com>
2026-05-26 11:21:02 +0530

    update api response dtos

---
+++ DTOs/ApiResponse.cs
@@ -0,0 +1,11 @@
+namespace EmployeeManagementAPI.DTOs
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public T? Data { get; set; }
+    }
+}
